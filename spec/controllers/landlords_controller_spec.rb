6e065a6b2a34aa31b9da66873a69d98e5f96cee3
James Lieu <j.lieu888@gmail.com>
2014-10-01 15:22:45 +0100

    Fix minor typo

--- spec/controllers/landlords_controller_spec.rb
+++ spec/controllers/landlords_controller_spec.rb
@@ -48,10 +48,10 @@ describe LandlordsController do
 
   describe "GET new" do
 
-    it "renders the index template" do
+    it "renders the new template" do
       get :new
       expect(response).to render_template("new")
     end
-  end # END GET index
+  end # END GET new
 
 end
