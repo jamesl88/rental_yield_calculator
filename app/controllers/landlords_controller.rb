b8208f165f2be546c601044d0ca314f29d1dd6e7
James Lieu <j.lieu888@gmail.com>
2014-10-07 00:46:17 +0100

    added pagination

--- app/controllers/landlords_controller.rb
+++ app/controllers/landlords_controller.rb
@@ -3,7 +3,7 @@ class LandlordsController < ApplicationController
   helper_method :sort_column, :sort_direction
 
   def index
-    @landlords = Landlord.order(sort_column + " " + sort_direction)
+    @landlords = Landlord.order(sort_column + " " + sort_direction).paginate(per_page: 6, page: params[:page])
   end
 
   def edit
